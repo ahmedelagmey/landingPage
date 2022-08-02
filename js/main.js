d110229b35fdb76ac03865cc387507d76a3844b3
ahmedelagmey <ahmedelajmey@gmail.com>
2022-08-02 16:40:43 +0200

    last update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.mainImg');
+let container = document.querySelector('.container');
+
+function changePhone(phone){
+    img.src = phone; 
+}
+
+function changeBackground(color){
+    container.style.background = color;
+
+}
