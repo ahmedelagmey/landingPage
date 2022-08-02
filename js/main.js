d919c6740aff97692e70c7a2dd127ea5c150b4eb
ahmedelagmey <ahmedelajmey@gmail.com>
2022-08-02 16:45:13 +0200

    function comments

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 let img = document.querySelector('.mainImg');
 let container = document.querySelector('.container');
 
-function changePhone(phone){
+function changePhone(phone){// changing main image in the content div
     img.src = phone; 
 }
 
-function changeBackground(color){
+function changeBackground(color){ // changing container background to phone color
     container.style.background = color;
 
 }
